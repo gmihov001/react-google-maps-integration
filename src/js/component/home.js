40dac7a4d8526f088cabc2e8b7b916b60aafa486
George Mihov <gmihov001@gmail.com>
2021-01-16 23:25:28 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,11 @@
 import React from "react";
+import { GoogleMap, Marker } from 'react-google-maps';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-export function Home() {
+export function Home(props) {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}></GoogleMap> 
 		</div>
 	);
 }
