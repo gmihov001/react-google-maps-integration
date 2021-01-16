58b4303c37b7caebf1a968392fe3a0a92d04defe
George Mihov <gmihov001@gmail.com>
2021-01-16 23:32:21 +0000

    Updates

--- src/js/component/MyMapComponent.js
+++ src/js/component/MyMapComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 import { GoogleMap, Marker } from 'react-google-maps';
 
 
-export function Home(props) {
+export const MyMapComponent = (props) => {
 	return (
 		<div className="text-center mt-5">
 			<GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
