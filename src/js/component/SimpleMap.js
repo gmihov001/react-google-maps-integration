9fedf6f31e3c392b5404c1db293e8dc7147a5153
George Mihov <gmihov001@gmail.com>
2021-01-17 00:52:46 +0000

    Updates

---
+++ src/js/component/SimpleMap.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+export class Home extends React.Component {
+    constructor(){
+        super(props);
+    }
+    render(){
+    return <div className="text-center mt-5" />;
+    }
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,7 @@
 import React from "react";
-import { GoogleMap, Marker } from "react-google-maps";
+import GoogleMapReact from "google-map-react";
 import { MyMapComponent } from "./MyMapComponent";
 
 export const Home = props => {
-	return (
-		<div className="text-center mt-5">
-			<MyMapComponent
-				googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-				loadingElement={<div style={{ height: `100%` }} />}
-				containerElement={<div style={{ height: `400px` }} />}
-				mapElement={<div style={{ height: `100%` }} />}
-			/>
-		</div>
-	);
+	return <div className="text-center mt-5" />;
 };
