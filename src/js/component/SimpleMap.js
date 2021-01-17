c9f076c57b456facccf1c04dd33e886cc1214de2
George Mihov <gmihov001@gmail.com>
2021-01-17 01:21:02 +0000

    Updates

--- src/js/component/SimpleMap.js
+++ src/js/component/SimpleMap.js
@@ -1,11 +1,38 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 
-export class Home extends React.Component {
+const API_KEY = process.env.REACT_APP_MAP_API_KEY; 
+
+const AnyReactComponent = ({text}) => <div>{text}</div>;
+
+export class SimpleMaps extends React.Component {
     constructor(){
         super(props);
     }
+
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    }
+
     render(){
-    return <div className="text-center mt-5" />;
+    return (
+    <div style={{ height: '100vh', width: '100%' }} className="text-center mt-5">
+        <GoogleMapReact 
+            bootstrapURLKeys={{key: API_KEY}}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+        >
+            <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+            />
+        </GoogleMapReact>    
+    </div>
+    );
     }
 };
