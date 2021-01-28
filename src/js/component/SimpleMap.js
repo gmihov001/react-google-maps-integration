3f26b267e88d736dfe2553b35fd3a6b0a7fbe390
George Mihov <gmihov001@gmail.com>
2021-01-28 17:42:43 +0000

    Updates

--- src/js/component/SimpleMap.js
+++ src/js/component/SimpleMap.js
@@ -1,38 +1,62 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
+import PropTypes from "prop-types";
 
-const API_KEY = process.env.REACT_APP_MAP_API_KEY; 
+const API_KEY = "";
 
-const AnyReactComponent = ({text}) => <div>{text}</div>;
+const Marker = ({ text }) => <div>{text}</div>;
 
 export class SimpleMaps extends React.Component {
-    constructor(){
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			lostPets: [],
+			foundPets: []
+		};
+	}
 
-    static defaultProps = {
-        center: {
-            lat: 59.95,
-            lng: 30.33
-        },
-        zoom: 11
-    }
+	static defaultProps = {
+		center: {
+			lat: 26.1723,
+			lng: -80.1319
+		},
+		zoom: 11
+	};
 
-    render(){
-    return (
-    <div style={{ height: '100vh', width: '100%' }} className="text-center mt-5">
-        <GoogleMapReact 
-            bootstrapURLKeys={{key: API_KEY}}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-        >
-            <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-            />
-        </GoogleMapReact>    
-    </div>
-    );
-    }
+	render() {
+		return (
+			<div
+				style={{ height: "100vh", width: "100%" }}
+				className="text-center mt-5">
+				<GoogleMapReact
+					bootstrapURLKeys={{ key: API_KEY }}
+					defaultCenter={this.props.center}
+					defaultZoom={this.props.zoom}
+					yesIWantToUseGoogleMapApiInternals
+					onGoogleApiLoaded={({ map, maps }) =>
+						apiIsLoaded(map, maps, places)
+					}>
+					{this.state.lostPets.map(() => (
+						<Marker
+							lat={59.955413}
+							lng={30.337844}
+							text="My Marker"
+						/>
+					))}
+				</GoogleMapReact>
+			</div>
+		);
+	}
+}
+
+Marker.propTypes = {
+	text: PropTypes.string,
+	center: PropTypes.object,
+	zoom: PropTypes.number
+};
+
+SimpleMaps.propTypes = {
+	text: PropTypes.string,
+	center: PropTypes.object,
+	zoom: PropTypes.number
 };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,12 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { MyMapComponent } from "./MyMapComponent";
+import { SimpleMaps } from "./SimpleMap";
 
 export const Home = props => {
-	return <div className="text-center mt-5" />;
+	return (
+		<div className="text-center mt-5">
+			<SimpleMaps />
+		</div>
+	);
 };
